0d1c229c0e527ef450b30233dc3c8810cf3a46e0
enginoobz <enginoobz@gmail.com>
2021-11-12 10:50:30 +0100

    Refactor: clarify statements

--- src/main/java/labs/lab5/Problem1.java
+++ src/main/java/labs/lab5/Problem1.java
@@ -10,9 +10,9 @@ public class Problem1 {
     public static void main(String[] args) {
         var wordsCounts = new HashMap<String, Integer>();
 
-        for (String word : scanStrings("Enter words: ")) {
-            Integer count = firstNonNull(wordsCounts.get(word), 0) + 1;
-            wordsCounts.put(word, count);
+        for (String word : scanStrings("Enter words (separated by whitespace: ")) {
+            Integer currentCount = firstNonNull(wordsCounts.get(word), 0);
+            wordsCounts.put(word, currentCount + 1);
         }
 
         log(wordsCounts);
