d38ab5e984f7158102982b33320fd6d9f300b51b
enginoobz <enginoobz@gmail.com>
2021-11-12 10:35:13 +0100

    Refactor

--- src/main/java/labs/lab5/Problem2.java
+++ src/main/java/labs/lab5/Problem2.java
@@ -1,21 +1,16 @@
 package labs.lab5;
 
-import java.util.Random;
-
 import static utils.LogUtils.*;
 import static utils.NumeralUtils.*;
 
 public class Problem2 {
-    static String template = "I will never spam my friends again.";
-
     public static void main(String[] args) {
+        String template = "I will never spam my friends again.";
+
         for (int i = 0; i < 100; i++) {
             char randomChar = (char) Random('a', 'z');
-            log(generateTypo(randomChar, Random(1, template.length() - 1)));
+            int randomPos = Random(1, template.length() - 1);
+            log(template.substring(0, randomPos) + randomChar + template.substring(randomPos));
         }
-    }
-
-    public static String generateTypo(char ch, int position) {
-        return template.substring(0, position) + ch + template.substring(position);
     }
 }
